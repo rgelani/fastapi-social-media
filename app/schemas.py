57c080acac23fb0336c80c15d7f14f24301a486b
rgelani <info.rgelani@gmail.com>
2025-11-22 18:20:25 -0500

    Add frontend

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,15 @@
 from pydantic import BaseModel
+from fastapi_users import schemas
+import uuid
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
 
 class PostCreate(BaseModel):
    title: str
